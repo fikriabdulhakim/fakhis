a37420384f0da7d5ca675074efc0a5d6295a145c
fikri abdulhakim <80445849+fikriabdulhakim@users.noreply.github.com>
2026-05-17 12:48:17 +0200

    Update URLs for Rheina and Kuisioner

--- core/views.py
+++ core/views.py
@@ -37,8 +37,8 @@ def home(request):
     ]
 
     software = [
-        {"name": "Rheina", "url": "https://www.rheina.id"},
-        {"name": "Kuisioner", "url": "https://www.kuisioner.com"},
+        {"name": "Rheina", "url": "https://rheina.id"},
+        {"name": "Kuisioner", "url": "https://kuisioner.com"},
 
     company = [
         {"name": "About Us", "url": google_link("About Us")},
