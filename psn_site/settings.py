5d54f8158df2e4bc9e6e6770b1c41e185a0f486c
fikri abdulhakim <80445849+fikriabdulhakim@users.noreply.github.com>
2026-05-17 09:30:52 +0200

    Configure static files settings in settings.py

--- psn_site/settings.py
+++ psn_site/settings.py
@@ -45,5 +45,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'core' / 'static']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
